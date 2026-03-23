804b6ca02111dbe85844802d57f8b7f073f4df9d
Greg Gant <me@greggant.com>
2026-03-23 10:24:26 -0700

    Minor responsive behavior tweak
    Connect/refresh collapse below 440px making for smaller screen support, target is down to 320px wide usable

--- hotline-tauri/src/components/tracker/TrackerWindow.tsx
+++ hotline-tauri/src/components/tracker/TrackerWindow.tsx
@@ -69,7 +69,7 @@ export default function TrackerWindow() {
             <svg className={`w-4 h-4 transition-transform duration-700 ease-in-out ${refreshing ? 'animate-spin' : ''}`} fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
             </svg>
-            <span>Refresh</span>
+            <span className="hidden min-[440px]:inline">Refresh</span>
           </button>
           <button
             onClick={() => setShowConnect(true)}
@@ -77,7 +77,7 @@ export default function TrackerWindow() {
             title="Connect to Server"
           >
             <span>🌐</span>
-            <span>Connect</span>
+            <span className="hidden min-[440px]:inline">Connect</span>
           </button>
           <button
             onClick={() => setShowSettings(true)}
